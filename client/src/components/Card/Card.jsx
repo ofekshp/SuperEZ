3d492c3f89db90c55b571b0e4f6ad36d9b4de6de
Ofek Shpirer <ofekshp2821@gmail.com>
2024-07-29 20:17:38 +0300

    work + -

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,52 +1,21 @@
-import React, { useState } from "react";
+// ProductCard.jsx
+import React from "react";
 import "../Card/Card.css";
 
-const ProductCard = ({ image, name, onAddToCart, onRemoveFromCart }) => {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-    onAddToCart();
-  };
-
-  const decrementCount = () => {
-    if (count > 0) {
-      setCount(count - 1);
-      onRemoveFromCart();
-    }
-  };
-
+const ProductCard = ({ image, name, count, onIncrement, onDecrement }) => {
   return (
     <div className="card">
       <img src={image} alt={name} />
       <div className="card-content">
         <h2>{name}</h2>
         <div className="buttons">
-          {/* <button onClick={decrementCount}>-</button> */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="1em"
             height="1em"
             viewBox="0 0 24 24"
-            onClick={incrementCount}
+            onClick={onDecrement}
             className="buttonAction"
-          >
-            <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeWidth={2}
-              d="M12 19V5m7 7H5"
-            ></path>
-          </svg>
-          <span>{count}</span>
-          {/* <button onClick={incrementCount}>+</button> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            onClick={decrementCount}
           >
             <path
               fill="none"
@@ -55,6 +24,23 @@ const ProductCard = ({ image, name, onAddToCart, onRemoveFromCart }) => {
               strokeLinejoin="round"
               strokeWidth={2}
               d="M5 12h14"
+            ></path>
+          </svg>
+          <span>{count}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            onClick={onIncrement}
+            className="buttonAction"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeWidth={2}
+              d="M12 19V5m7 7H5"
             ></path>
           </svg>
         </div>
