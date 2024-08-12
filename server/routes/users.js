2ad41889d95512e76c5c7663cc80c82d97bfaa1e
AdiNehama <adinehama6@gmail.com>
2024-08-12 16:17:16 +0300

    added the 'my profile' modal and fixed buttons

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,22 +4,42 @@ const { registerUser } = require('../controllers/users/userController.js');
 const { signInUser } = require('../controllers/users/loginController.js');
 const { logout } = require('../controllers/users/logoutController.js');
 const { resetPassword } = require('../controllers/users/forgotPasswordController.js');
+const { getUserProfile, updateUserProfile } = require('../controllers/users/myProfileController.js');
 
+// Route for user registration
 router.post('/signup', (req, res) => {
   console.log('Received POST request to /signup');
   registerUser(req, res);
 });
 
+// Route for user login
 router.post('/login', (req, res) => {
   console.log('Received POST request to /login');
   signInUser(req, res);
 });
 
-router.post('/logout', logout);
+// Route for user logout
+router.post('/logout', (req, res) => {
+  console.log('Received POST request to /logout');
+  logout(req, res);
+});
 
+// Route for password reset
 router.post('/reset-password', (req, res) => {
   console.log('Received POST request to /reset-password');
   resetPassword(req, res);
+});
+
+// Route for fetching user profile
+router.get('/profile', (req, res) => {
+  console.log('Received GET request to /profile');
+  getUserProfile(req, res);
+});
+
+// Route for updating user profile
+router.put('/profile', (req, res) => {
+  console.log('Received PUT request to /profile');
+  updateUserProfile(req, res);
 });
 
 module.exports = router;
