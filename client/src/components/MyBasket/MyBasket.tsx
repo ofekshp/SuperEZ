c942b74cf0b99ad786064e0a64000c304c06871e
Ofek Shpirer <ofekshp2821@gmail.com>
2024-08-22 17:50:30 +0300

    fix

--- client/src/components/MyBasket/MyBasket.tsx
+++ client/src/components/MyBasket/MyBasket.tsx
@@ -261,82 +261,12 @@ const MyBasket: React.FC = () => {
                 setIsUserLoggedIn={setIsUserLoggedIn}
               />)}
             </div>
-            <div>
-              <label htmlFor="phone" className="my-basket__form-label">טלפון:</label>
-              <input
-                id="phone"
-                type="text"
-                className="my-basket__form-input"
-                placeholder="הכנס טלפון"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="fullName" className="my-basket__form-label">שם מלא:</label>
-              <input
-                id="fullName"
-                type="text"
-                className="my-basket__form-input"
-                placeholder="הכנס שם מלא"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="city" className="my-basket__form-label">עיר:</label>
-              <input
-                id="city"
-                type="text"
-                className="my-basket__form-input"
-                placeholder="הכנס עיר"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="streetAndHouse" className="my-basket__form-label">רחוב ומספר בית:</label>
-              <input
-                id="streetAndHouse"
-                type="text"
-                className="my-basket__form-input"
-                placeholder="הכנס רחוב ומספר בית"
-                value={streetAndHouse}
-                onChange={(e) => setStreetAndHouse(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="floorNumber" className="my-basket__form-label">מספר קומה:</label>
-              <input
-                id="floorNumber"
-                type="text"
-                className="my-basket__form-input"
-                placeholder="הכנס מספר קומה"
-                value={floorNumber}
-                onChange={(e) => setFloorNumber(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="apartmentNumber" className="my-basket__form-label">מספר דירה:</label>
-              <input
-                id="apartmentNumber"
-                type="text"
-                className="my-basket__form-input"
-                placeholder="הכנס מספר דירה"
-                value={apartmentNumber}
-                onChange={(e) => setApartmentNumber(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="my-basket__login-prompt">
-            <span>רוצים לשמור את הסל שלכם?  הרשמו / התחברו </span>
-            {signUpOpen && <SignUpModal closeModal={closeModal} />}
-          </div>
           <div>
           <button className="my-basket__order-button" onClick={handleSubmit}>הזמן עכשיו</button>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
